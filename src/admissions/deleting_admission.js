6162daa5fca5f5ae0b38468286f41d35b8527191
GuptaNItesh01 <guptanitesh0102@gmail.com>
2024-12-11 21:21:33 +0530

    apicalling

---
+++ src/admissions/deleting_admission.js
@@ -0,0 +1,23 @@
+async function main (req,res){
+   
+    const admission_id=req.query.admission_id;
+
+       const {getPGConnection} = require("../base/pg_connector")
+       const client =await getPGConnection();
+       console.log("CLIENT",client)
+       
+          await client.query(`DELETE FROM admissions WHERE admission_id=$1;`, [admission_id],async function(err,data){
+              if(data){
+                  res.send(' delete successfully');
+              }
+              else{
+                  res.send(err);
+                  console.log(err);
+              }
+              await client.end() 
+          })
+    
+}
+module.exports={
+    main
+}
---
+++ src/branches/creating_branches.js
@@ -0,0 +1,38 @@
+
+async function main(req,res){
+    const branch_id=req.query.branch_id;
+    const branch_name=req.query.branch_name; 
+    const branch_code=req.query.branch_code;
+    const location=req.query.location;
+    const student_id=req.query.student_id;
+    const head_of_branch=req.query.head_of_branch;
+    const contact_number=req.query.contact_number;
+
+    console.log(branch_id,branch_name, branch_code ,location,student_id,head_of_branch,contact_number);
+
+
+const {getPGConnection}=require('../base/pg_connector');
+let  client=await getPGConnection();
+const result= await client.query(' INSERT INTO branches ( branch_id,branch_name, branch_code ,location,student_id,head_of_branch,contact_number)  values($1,$2,$3,$4,$5,$6,$7)', 
+    [ branch_id,branch_name, branch_code ,location,student_id,head_of_branch,contact_number]
+    ,function(err,data){
+    if(err){
+        console.log("Error",err);
+        res.send(`ERROR in inserting the vlaue:-  ${err}`);
+    } else{
+        if(res.send){
+            res.send(`Data inserted successfully:- ${JSON.stringify(result)}`)
+        }
+    }
+
+     client.end();
+}
+);
+
+}
+module.exports={
+    main
+}
+
+
+
